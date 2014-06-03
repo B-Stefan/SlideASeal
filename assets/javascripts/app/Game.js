bb166d8c3b4e3b426060890a805bbf94fdc31fe3
faBimon <fjunge@fabimon.org>
2014-06-03 13:02:51 +0200

    merge

--- assets/javascripts/app/Game.js
+++ assets/javascripts/app/Game.js
@@ -2,13 +2,13 @@
  * Created by Besitzer on 14.05.2014.
  */
 
-define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield'],function (Phaser, $, Panel, network, _,Gamefield){
-
+define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield', 'app/NextPanelIndikator'],function (Phaser, $, Panel, network, _,Gamefield, NextPanelIndikator){
     var shipStripe;
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamefield', { preload: preload, create: create, update: update}, true);
     var name = "Peter";
 
-    var gamefield;
+    var gamefield,
+        panelindikator;
 
     var sessionid = $("#sessionid").text();         // in the future parsed from the dom 
 
@@ -18,14 +18,15 @@ define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield'],function
         game.load.image('ship','../Images/Schiff.svg');
 
         // establish Network Connection 
-
         network.addGameStartEventListener(handelGameStart);     // is called when the game starts
         network.addNewGameStateEventListener(handelGameState);  // is called when a new GameState arrives
         network.addScoreEventListener(handelScore);             // is called when new Score information are available
         network.addDisconnectEventListener(handelDisconnect);   // is called when a disconnect happend
         network.register(name, sessionid);                      // register the client at the server and join a session
 
+        game.load.image('NextPanelIndikator','../Images/nip.png');
         Panel.loadAllTypes(game)
+
         game.load.spritesheet('Robbe', '../Images/Robbe.png', 520, 520, 17);
         game.load.spritesheet('Robbe2', '../Images/RobbeBall.png', 520, 520, 18);
     }
@@ -34,6 +35,8 @@ define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield'],function
         game.physics.startSystem(Phaser.Physics.P2JS);
         shipStripe = game.add.sprite(game.world.width, 80, 'ship');
         gamefield = new Gamefield(game)
+        panelindikator = new NextPanelIndikator(game);
+
 
         //gamefield.enableBodyDebug = true;
         test3 = game.add.sprite(300, 180, 'Panel_Background');
@@ -66,9 +69,7 @@ define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield'],function
     }
 
     function update(){
-
-
-
+        panelindikator.update();
     }
 
     // handel Slide
@@ -97,6 +98,7 @@ define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield'],function
             console.log("| " + data.field[i][0] + " | " + data.field[i][1] + " | " + data.field[i][2] + " | " + data.field[i][3] + " | " + data.field[i][4] + " |")
             console.log("|---|---|---|---|---|")
         }
+        panelindikator.setNextPanels(data.nextPanels);
     }
 
    // handel Score
@@ -104,7 +106,6 @@ define(['Phaser', 'jquery', './Panel', 'network', '_', 'app/Gamefield'],function
         console.log('!!! Score !!!');
         console.log("you score is: " + data.you.score);
         console.log("rival score is: " + data.rival.score);
-        //console.log(data);
     }
 
     // handel Disconnect
